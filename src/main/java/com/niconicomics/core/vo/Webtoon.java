0e76f29c4893fde3bb06e2ee55c2540233c54b81
ggerf <roto0724@gmail.com>
2020-03-16 18:35:28 +0900

    webtoon, episode, contents 입력 및 값 불러오기(contents는 아직 미완성)

---
+++ src/main/java/com/niconicomics/core/vo/Webtoon.java
@@ -0,0 +1,18 @@
+package com.niconicomics.core.vo;
+
+import lombok.Data;
+
+@Data
+public class Webtoon {
+
+	private int webtoonId;
+	private String title;
+	private String summary;
+	private int authorId;
+	private String hashtag;
+	private String mgrHashtag;
+	private String thumbnail;
+	private int hits;
+	private String end;
+	
+}
