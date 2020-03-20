4bebe2fa36c4b1bb890894c9872984583a2b756b
ggerf <roto0724@gmail.com>
2020-03-20 17:53:13 +0900

    2020/03/20작업분 스플릿태그 작업중

--- src/main/java/com/niconicomics/core/util/SplitTag.java
+++ src/main/java/com/niconicomics/core/util/SplitTag.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
 public class SplitTag {
 	
-	public ArrayList<String> splitTag(String tag) {
+	public static ArrayList<String> splitTag(String tag) {
 		String[] splitTag = tag.split("#");
-		ArrayList<String> transTag = new ArrayList<String>();
+		ArrayList<String> transTagList = new ArrayList<String>();
 		for (int i = 0; i < splitTag.length; i++) {
 			transTag.add(splitTag[i]);
 		}
